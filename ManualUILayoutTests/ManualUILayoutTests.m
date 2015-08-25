49a883519e64b8bf8e55dbe3fadd624b6afdecfd
Alexey Klyotzin <aklyotzin@oysterlabs.com>
2015-08-25 10:36:54 +0300

    Initial Commit

---
+++ ManualUILayoutTests/ManualUILayoutTests.m
@@ -0,0 +1,40 @@
+//
+//  ManualUILayoutTests.m
+//  ManualUILayoutTests
+//
+//  Created by Alexey Klyotzin on 25/08/15.
+//  Copyright (c) 2015 Test projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ManualUILayoutTests : XCTestCase
+
+@end
+
+@implementation ManualUILayoutTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
